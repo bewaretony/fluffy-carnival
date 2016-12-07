64dc94da708c320f1eda2136d37d47cfcbd48584
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-12-06 20:17:18 -0700

    Initial commit

---
+++ Assets/Spinner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.localRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, 1f, 0));
+	}
+}
